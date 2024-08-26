5eff1895b42174d45087e7165334cef1fa3111f2
Swapnil Sharma Sarker <swapnilsharmasarker@gmail.com>
2024-08-26 16:26:00 +0600

    add projects

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrl: './education.component.scss'
+  styleUrl: './education.component.scss',
 })
 export class EducationComponent {
-
+  education = [
+    {
+      title: 'High School',
+      institution: 'Goverment Laboratory High School',
+      year: '2006 - 2018',
+      logo: '../../assets/school-logo.png',
+      description:
+        'Completed high school with a focus on science, participating in various Olympiads and problem-solving competitions.',
+    },
+    {
+      title: "Bachelor's Degree in Computer Science",
+      year: '2019 - 2023',
+      institution: 'Ahsanullah University of Science and Technology',
+      logo: '../../assets/varsity-logo.png',
+      description:
+        "Pursued a Bachelor's degree in Computer Science, winning university programming contests and conducting research in NLP, Computer Vision, and Bioinformatics.",
+    },
+  ];
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      title: 'Leave Portal',
+      description:'',
+      affiliation:'Enosis Solutions',
+      details: [
+        'Built with Angular in the frontend and Spring in the backend',
+        'Integrated state management replacing existing communication service',
+        'Built reusable angular components',
+        'Used Reactive approach (Observables) for calling API and inter-component communication'
+      ]
+    },
 
+    {
+      title: 'Community Voting System',
+      description: '',
+      affiliation: 'Enosis Solutions',
+      details: [
+        'Most of the services are built with RestEasy',
+        'Uses docker and AWS',
+        'Frontend is built with Angular',
+        'Used Design patterns like singleton, factory, observer, strategy',
+        'Learned concurrency and transaction management of a singleton bean',
+        'Learned to write reusable codes that can be exported as libraries.'
+      ]
+    }
+  ];
 }
