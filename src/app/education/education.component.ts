8de8918112ba5f8fbf5b87da67a239695ea7173d
Swapnil Sharma Sarker <swapnilsharmasarker@gmail.com>
2024-08-26 22:15:46 +0600

    fix varsity logo issue

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -19,7 +19,7 @@ export class EducationComponent {
       title: "Bachelor's Degree in Computer Science",
       year: '2019 - 2023',
       institution: 'Ahsanullah University of Science and Technology',
-      logo: '../../assets/varsity-logo.png',
+      logo: 'assets/varsity-logo.png',
       description:
         "Pursued a Bachelor's degree in Computer Science, winning university programming contests and conducting research in NLP, Computer Vision, and Bioinformatics.",
     },
