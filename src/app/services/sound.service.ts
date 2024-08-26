ff89f8586fcfea93808cf4bd5fdf55fe0e6ada09
Swapnil Sharma Sarker <swapnilsharmasarker@gmail.com>
2024-08-26 22:09:47 +0600

    improve sound performance

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root',
 })
 export class SoundService {
+  private audio: HTMLAudioElement | null = null;
+
+  constructor() {
+    if (typeof Audio !== 'undefined') {
+      this.audio = new Audio('assets/mouse-click-sound-233951.mp3');
+      this.audio.load();
+    }
+  }
+
   playClickSound() {
-    let audio = new Audio();
-    audio.src = 'assets/mouse-click-sound-233951.mp3';
-    audio.load();
-    audio.play();
+    if (this.audio) {
+      this.audio.currentTime = 0;
+      this.audio
+        .play()
+        .catch((error) => console.error('Error playing audio:', error));
+    } else {
+      console.warn('Audio playback is not supported in this environment');
+    }
   }
 }
