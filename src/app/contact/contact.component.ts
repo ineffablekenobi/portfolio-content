aefcb00023262cf40b7a1eb311c7550986a988d4
Swapnil Sharma Sarker <swapnilsharmasarker@gmail.com>
2024-08-28 23:10:19 +0600

    add email sender

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-contact',
@@ -12,7 +14,44 @@ export class ContactComponent {
     message: '',
   };
 
+  constructor(private messageService: MessageService) {}
+
   sendEmail(): void {
-    console.log('Yet to implement');
+    const templateParams = {
+      from_name: this.contactForm.name,
+      from_email: this.contactForm.email,
+      message: this.contactForm.message,
+    };
+
+    emailjs
+      .send(
+        'service_rhfmjii',
+        'template_ro3zmfo',
+        templateParams,
+        'Rm-eoqw2KFad4BxGp'
+      )
+      .then(
+        (response: EmailJSResponseStatus) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Email was sent Successfully',
+            detail:
+              "Thank your for contacting me. You'll hear back from me soon.",
+          });
+          // Optionally, reset the form here
+          this.contactForm = {
+            name: '',
+            email: '',
+            message: '',
+          };
+        },
+        (error) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: "Email couldn't be sent",
+            detail: 'Please contact swapnilsharmasarker@gmail.com',
+          });
+        }
+      );
   }
 }
