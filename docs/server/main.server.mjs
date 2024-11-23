e43e9a874a1d7a2f1a096ff84fd8709b578283ca
Swapnil Sharma Sarker <swapnilsharmasarker@gmail.com>
2024-11-23 13:07:30 +0600

    remove ssr

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BYMMHMR4.mjs";import"./chunk-QALAH7SJ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-LREBEUX5.mjs";import"./chunk-2TLH4BHK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
