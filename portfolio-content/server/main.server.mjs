73113b4ec432c4943b05f9ecee0a006d9f7ca74f
Swapnil Sharma Sarker <swapnilsharmasarker@gmail.com>
2024-11-23 13:02:08 +0600

    Auto-generated commit

--- portfolio-content/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-LREBEUX5.mjs";import"./chunk-2TLH4BHK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-GEMA6NKD.mjs";import"./chunk-2TLH4BHK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
