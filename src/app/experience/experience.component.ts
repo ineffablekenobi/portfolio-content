2a2d016553dcb7a0a13fdc94ed758262678882f1
Swapnil Sharma Sarker <swapnilsharma@ad.enosisbd.com>
2026-03-14 00:25:27 +0600

    fix build issues

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SoundService } from '../services/sound.service';
+import { EventBusService } from '../services/event-bus.service';
+import { Subscription } from 'rxjs';
+import { EVENT_NAMES } from '../constants/event-constants';
 
 @Component({
   selector: 'app-experience',
@@ -37,7 +41,7 @@ export class ExperienceComponent {
     }
   ];
 
-  constructor(private eventBus: EventBusService) { }
+  constructor(private eventBus: EventBusService) {}
 
   playClickSound(): void {
     this.eventBus.cast(EVENT_NAMES.CLICK);
